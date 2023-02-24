b60a659530b475f40eeb9de14a36fd8b7a29d0b2
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-02-24 15:19:32 +0200

    Set up backend app
    .env variable PORT does not work. is env not importing correctly or what?

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,34 @@
-const express = require("express");
+import express from "express";
+import chats from "./data/data.js";
+import dotenv from 'dotenv';
 
+//--- App Config
 const app = express();
+dotenv.config();
 
+//--- Middlewares
+
+
+//--- DB config
+
+
+//--- API endpoints
 app.get('/', (req, res) => {
 	res.send("API is running"); //This sends an HTTP response back to the client
 });
 
+app.get('/api/chat', (req, res) => {
+	// res.send(chats);
+});  
 
-app.listen(5000, console.log("Server started on PORT 5000"));
+app.get('/api/chat/:id', (req, res) => {
+	// console.log(req.params);
+	const singleChat = chats.find((chat) => chat._id === req.params.id);
+	console.log(singleChat);
+	res.send(singleChat);
+});
+
+
+const PORT = process.env.PORT || 5000; // .env variable PORT not working
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
