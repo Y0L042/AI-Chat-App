8ddc699de3603e6fc5f44eeb3799e990c6319af9
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-02-27 14:52:10 +0200

    Added search for user functionality w auth

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { registerUser, authUser } from "../controllers/userControllers.js";
+import { registerUser, authUser, allUsers } from "../controllers/userControllers.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').post(registerUser);
+// routes. Controllers like registerUser & allUsers is in userControllers.js
+router.route('/').post(registerUser).get(protect, allUsers);
 router.post('/login', authUser);
 
 export default router;
