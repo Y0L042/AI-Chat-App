9986f2dc164def21eec6cf31891b3587bb138b01
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-02-27 17:26:09 +0200

    Created add and remove user from group API
    Add check if user is in group before adding (prevent multiple addings)

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -5,6 +5,8 @@ import {
   fetchChats,
   createGroupChat,
   renameGroup,
+  removefromGroup,
+  addToGroup
 } from "../controllers/chatControllers.js";
 
 const router = express.Router();
@@ -14,7 +16,7 @@ router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
-// router.route('/groupremove').put(protect, removefromGroup);
-// router.route('/groupadd').put(protect, addToGroup);
+router.route('/groupremove').put(protect, removefromGroup);
+router.route('/groupadd').put(protect, addToGroup);
 
 export default router;
