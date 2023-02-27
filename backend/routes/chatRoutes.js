9822f70d7856b90db45126bb2ac7e9a665231ecc
Y0L042 <66077762+Y0L042@users.noreply.github.com>
2023-02-27 16:15:20 +0200

    Created fetch user chats API
    Also Prettier'd some files

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { accessChat } from "../controllers/chatControllers.js";
+import { accessChat, fetchChats } from "../controllers/chatControllers.js";
 
 const router = express.Router();
 
 // routes for accessing or creating chats
-router.route('/').post(protect, accessChat);
-// router.route('/').get(protect, fetchChats);
+router.route("/").post(protect, accessChat);
+router.route("/").get(protect, fetchChats);
 // router.route('/group').post(protect, createGroup);
 // router.route('/rename').put(protect, renameGroup);
 // router.route('/groupremove').put(protect, removefromGroup);
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import express from "express";
 import chats from "./data/data.js";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 // Superfluous
-import colors from 'colors';
+import colors from "colors";
 
 //--- App Config
 dotenv.config();
@@ -16,26 +16,22 @@ const app = express();
 app.use(express.json()); // to accept JSON data
 //--- Middlewares
 
-
 //--- DB config
-
 
 //--- API endpoints
 
-	// endpoints gets called in sequence
+// endpoints gets called in sequence
 
-app.get('/', (req, res) => {
-	res.send("API is running"); //This sends an HTTP response back to the client
+app.get("/", (req, res) => {
+  res.send("API is running"); //This sends an HTTP response back to the client
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 // Error handling middleware
 app.use(notFound); // handles not found errors
 app.use(errorHandler); // catches all other error codes
 
-
 const PORT = process.env.PORT || 5000; // .env variable PORT not working
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`.yellow.bold));
-
